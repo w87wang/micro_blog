344657bbd2868c1eb267a20a076619ba5afb0f10
Aaron Wang <w87wang@uwaterloo.ca>
2014-11-19 14:23:21 -0800

    microblog setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = 'e323e4fc2781e55a4c2d9431fc502334aca71a63f697b2ca019d04ea87461212ba3e2c9cc5acd90098f9a1617489b26e4c3fb921704e291a309ccd2e6e8c1ec2'
